c020c0bc700f55ebe1276b5b578dc46f4ee74ba4
goyalritesh21 <goyalritesh21@gmail.com>
2019-12-11 01:38:58 +0530

    Addition of assessment test

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic import FormView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html"))
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('assessment/', TemplateView.as_view(template_name="dass21.html")),
+    path('about/', TemplateView.as_view(template_name="about.html")),
+    path('blog-home', TemplateView.as_view(template_name="blog-home.html")),
+    path('blog-details', TemplateView.as_view(template_name="blog-details.html")),
+    path('contact', TemplateView.as_view(template_name="contact.html")),
 ]
