f124ebd4b1d7aa5305fa8fff140fdd6cf617fd9e
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-12-15 12:58:19 +0530

    models updated to include hindi text

--- assessment/models.py
+++ assessment/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Question(models.Model):
-    question = models.CharField(max_length=200, verbose_name="Question")
+    english_text = models.CharField(max_length=200, verbose_name="Question(English)")
+    hindi_text = models.CharField(max_length=200, verbose_name="Question(Hindi)")
 
     def __str__(self):
-        return self.question
+        return self.english_text
 
 
 class Choice(models.Model):
