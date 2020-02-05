e774ad9268242f463dbfdd819b36ca93e741b34d
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2020-02-05 16:44:17 +0530

    added dev_settings file

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,6 +1,7 @@
 from .base_settings import *
 
 if os.environ.get('DEV_MODE', 'True') == 'True':
+    print(os.environ.get('DEV_MODE', 'True'))
     from .dev_settings import *
 else:
     from .production_settings import *
