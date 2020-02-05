7ab9dd40a714129f6b2ad822cf892e98ff3f6ce3
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2020-02-05 16:47:25 +0530

    DEV_MODE variable defined

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,7 +1,6 @@
 from .base_settings import *
 
-if os.environ.get('DEV_MODE', 'True') == 'True':
-    print(os.environ.get('DEV_MODE', 'True'))
+if config('DEV_MODE') == 'True':
     from .dev_settings import *
 else:
     from .production_settings import *
