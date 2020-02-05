532b6fd61020d1cc042cb60cf993d053d4280b51
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2020-02-05 16:51:56 +0530

    refactor prod db settings

--- src/settings/production_settings.py
+++ src/settings/production_settings.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ['thecalmminds.org', '127.0.0.1', 'localhost']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DATABASE_USER'),
+        'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': 'localhost',
